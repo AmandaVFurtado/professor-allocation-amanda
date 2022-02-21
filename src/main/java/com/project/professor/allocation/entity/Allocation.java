683b1fdbc8ca6ef08f34cdef78d464f339b12f85
Amanda approved! <vieira.a.amanda@gmail.com>
2022-02-21 23:15:21 +0000

    Add repository layer

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -17,118 +17,104 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class Allocation {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private DayOfWeek day;
+
 	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
 	private Date start;
+
 	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
 	private Date end;
-	
+
 	@Column(name = "course_id", nullable = false)
 	private Long courseId;
+
 	public Course getCourse() {
 		return course;
 	}
-
 
 	public void setCourse(Course course) {
 		this.course = course;
 	}
 
-
 	public Professor getProfessor() {
 		return professor;
 	}
-
 
 	public void setProfessor(Professor professor) {
 		this.professor = professor;
 	}
 
-
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "course_id", insertable = false, updatable = false, nullable = false)
 	private Course course;
-	
+
 	@Column(name = "professor_id", nullable = false)
 	private Long professorId;
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "professor_id", insertable = false, updatable = false, nullable = false)
 	private Professor professor;
-	
-	
+
 	@Override
 	public String toString() {
 		return "Allocation [id=" + id + ", day=" + day + ", start=" + start + ", end=" + end + ", courseId=" + courseId
 				+ ", professorId=" + professorId + "]";
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public DayOfWeek getDay() {
 		return day;
 	}
-
 
 	public void setDay(DayOfWeek day) {
 		this.day = day;
 	}
 
-
 	public Date getStart() {
 		return start;
 	}
-
 
 	public void setStart(Date start) {
 		this.start = start;
 	}
 
-
 	public Date getEnd() {
 		return end;
 	}
-
 
 	public void setEnd(Date end) {
 		this.end = end;
 	}
 
-
 	public Long getCourseId() {
 		return courseId;
 	}
-
 
 	public void setCourseId(Long courseId) {
 		this.courseId = courseId;
 	}
 
-
 	public Long getProfessorId() {
 		return professorId;
 	}
 
-
 	public void setProfessorId(Long professorId) {
 		this.professorId = professorId;
 	}
-	
 
 }
--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -14,51 +14,64 @@ public class Professor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(nullable = false)
 	private String name;
+
 	@Column(length = 11, unique = true, nullable = false)
 	private String cpf;
-	
+
 	@Column(name = "department_id", nullable = false)
 	private Long departmentId;
-	
+
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "department_id", updatable = false, insertable = false, nullable = false)
 	private Department depart;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Long getDepartmentId() {
 		return departmentId;
 	}
+
 	public void setDepartmentId(Long departmentId) {
 		this.departmentId = departmentId;
 	}
+
 	public Department getDepart() {
 		return depart;
 	}
+
 	public void setDepart(Department depart) {
 		this.depart = depart;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public Long getDepartmentid() {
 		return departmentId;
 	}
+
 	public void setDepartmentid(Long departmentid) {
 		this.departmentId = departmentid;
 	}
