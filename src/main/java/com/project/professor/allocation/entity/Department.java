84c257d612d3c5923f6d60265da0efc6de983adc
Amanda approved! <vieira.a.amanda@gmail.com>
2022-03-09 20:36:04 +0000

    update

--- src/main/java/com/project/professor/allocation/entity/Department.java
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -1,20 +1,34 @@
 package com.project.professor.allocation.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Department {
 
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(unique = true, nullable = false)
 	private String name;
+	
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @OneToMany(mappedBy = "department")
+    private List<Professor> professors;
 
 	public Long getId() {
 		return id;
@@ -30,6 +44,12 @@ public class Department {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public void setProfessors(List<Professor> professors) {
+		this.professors = professors;
+		
+		
 	}
 
 }
