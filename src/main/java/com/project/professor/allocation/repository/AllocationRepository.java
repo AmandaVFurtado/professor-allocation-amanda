77c4c453a67457b53405275d4b2ebf3580d3d415
Amanda approved! <vieira.a.amanda@gmail.com>
2022-03-09 19:10:01 +0000

    update

--- src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/repository/AllocationRepository.java
@@ -2,6 +2,7 @@ package com.project.professor.allocation.repository;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.professor.allocation.entity.Allocation;
