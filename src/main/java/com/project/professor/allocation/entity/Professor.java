432304d9058b3d3354b44542b98aebcfc3dab5a8
Amanda approved! <vieira.a.amanda@gmail.com>
2022-03-09 20:14:10 +0000

    update

--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -1,5 +1,7 @@
 package com.project.professor.allocation.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,10 +9,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Professor {
 
+	@JsonProperty
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,12 +30,20 @@ public class Professor {
 	@Column(length = 11, unique = true, nullable = false)
 	private String cpf;
 
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@Column(name = "department_id", nullable = false)
 	private Long departmentId;
 
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "department_id", updatable = false, insertable = false, nullable = false)
 	private Department depart;
+	
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@OneToMany(mappedBy = "professor")
+	private List<Allocation> allocations;
 
 	public Long getId() {
 		return id;
@@ -74,5 +91,10 @@ public class Professor {
 
 	public void setDepartmentid(Long departmentid) {
 		this.departmentId = departmentid;
+	}
+
+	public void setAllocations(List<Allocation> allocations) {
+		this.allocations = allocations;
+		
 	}
 }
