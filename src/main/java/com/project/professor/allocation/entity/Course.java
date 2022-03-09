4d7c4c289af74ef0a97a049b5b49fde68dc2e98d
Amanda approved! <vieira.a.amanda@gmail.com>
2022-03-09 20:03:46 +0000

    update

--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,20 +1,34 @@
 package com.project.professor.allocation.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Course {
 
+	@JsonProperty
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false, unique = true)
 	private String name;
+	
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@OneToMany(mappedBy = "course")
+	private List<Allocation> allocations;
 
 	public Long getId() {
 		return id;
@@ -30,6 +44,13 @@ public class Course {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	
+
+	public void setAllocations(List<Allocation> allocations) {
+		this.allocations = allocations;
+		
 	}
 
 }
