538e689b2757982c8c695e32702693d7b442fecf
Amanda approved! <vieira.a.amanda@gmail.com>
2022-03-09 18:59:18 +0000

    update DepartmentRepository interface

--- src/main/java/com/project/professor/allocation/repository/DepartmentRepository.java
+++ src/main/java/com/project/professor/allocation/repository/DepartmentRepository.java
@@ -1,5 +1,8 @@
 package com.project.professor.allocation.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.professor.allocation.entity.Department;
@@ -7,5 +10,11 @@ import com.project.professor.allocation.entity.Department;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+	
+    List<Department> findByNameContainingIgnoreCase(String name);
+	
+	Optional<Department> findByName(String name);
+	
+	Optional<Department> findBySigla(String sigla);
 	
 }
