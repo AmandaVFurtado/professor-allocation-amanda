1d1d69af79ee08cc1f018362203af02dd7d398b1
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2022-03-11 20:33:59 -0300

    Remove definição de método não utilizado

--- src/main/java/com/project/professor/allocation/repository/CourseRepository.java
+++ src/main/java/com/project/professor/allocation/repository/CourseRepository.java
@@ -1,7 +1,6 @@
 package com.project.professor.allocation.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,8 +10,5 @@ import com.project.professor.allocation.entity.Course;
 public interface CourseRepository extends JpaRepository<Course, Long> {
 	
     List<Course> findByNameContainingIgnoreCase(String name);
-	
-	Optional<Course> findByName(String name);
-	
 
 }
